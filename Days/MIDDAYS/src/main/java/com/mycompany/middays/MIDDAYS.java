b494c01c52794f4eb98097bde9b278d3e71c9c05
riddham28 <riddhamavani7770@gmail.com>
2024-06-18 20:27:20 -0400

    merging done

---
+++ Days/MIDDAYS/src/main/java/com/mycompany/middays/MIDDAYS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.middays;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MIDDAYS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
